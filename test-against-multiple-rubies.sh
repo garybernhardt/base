bcf30f8d3edcaeb5d9d66c122e583b37d8369229
Gary Bernhardt <gary.bernhardt@gmail.com>
2011-09-09 16:28:30 -0700

    simplify running specs over multiple rubies

--- test-against-multiple-rubies.sh
+++ test-against-multiple-rubies.sh
@@ -2,13 +2,5 @@
 
 set -e
 
-rspec_versions="
-    $HOME/.rvm/gems/ree-1.8.7-2011.03@base/bin/rspec
-    $HOME/.rvm/gems/ruby-1.9.2-p180@base/bin/rspec
-    "
-for rspec_path in $rspec_versions; do
-    echo "Running specs with ${rspec_path}"
-    echo
-    $rspec_path spec
-done
+rvm ree@base,1.9.2-p180@base exec rspec spec
 
