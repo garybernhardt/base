21f9acd740160e0961e0b93634df749330bd7cee
Gary Bernhardt <gary.bernhardt@gmail.com>
2011-09-08 20:43:10 -0700

    fix rvm use in multiple ruby version test script

--- test-against-multiple-rubies.sh
+++ test-against-multiple-rubies.sh
@@ -2,5 +2,13 @@
 
 set -e
 
-(rvm use ree@base && rspec spec)
-(rvm use ruby-1.9.2-p180@base && rspec spec)
+rspec_versions="
+    $HOME/.rvm/gems/ree-1.8.7-2011.03@base/bin/rspec
+    $HOME/.rvm/gems/ruby-1.9.2-p180@base/bin/rspec
+    "
+for rspec_path in $rspec_versions; do
+    echo "Running specs with ${rspec_path}"
+    echo
+    $rspec_path spec
+done
+
